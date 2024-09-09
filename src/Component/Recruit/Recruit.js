3d8d74b85dde8bbfe2835566b134fd44580fa4e8
Song307 <ssongsyj0203@gmail.com>
2024-09-09 17:54:39 +0900

    update

--- src/Component/Recruit/Recruit.js
+++ src/Component/Recruit/Recruit.js
@@ -19,11 +19,39 @@ function Recruit(){
             <Navigator_Recruit/>
             <div className="Recruit" >
             <h2 data-aos="fade-right" data-aos-once='false'>Recruit</h2>
-            <form className='form' data-aos="fade-right" data-aos-once='false'>
-                <div className='form_list'> 이름 : <input type="text" placeholder="이름" className='form_text'/> </div>
-                <div className='form_list'> 학년 : 1학년<input type="radio"/> </div>
+            <form className='form' data-aos="fade-right" data-aos-once='true'>
+                <div className='form_list'> 이름 <br/>
+                    <input type="text" placeholder="이름" className='form_text'/>
+                </div>
+                <div className='form_list'> 학번 <br/>
+                    <input type="text" placeholder="학번" className='form_text'/>
+                </div>
+                <div className='form_list'> 학년
+                    <div className='form_input_list'>
+                    <input className='radio_text' type="radio" name='g'/>1학년<br/>
+                    <input className='radio_text' type="radio" name='g'/>2학년
+                    </div>
+                </div>
+                <div className='form_list'> 분반<br/>
+                    <div className='form_input_list'>
+                        <input className='radio_text' type="radio" name='y'/>YA<br/>
+                        <input className='radio_text' type="radio" name='y'/>YB<br/>
+                        <input className='radio_text' type="radio" name='y'/>YC<br/>
+                        <input className='radio_text' type="radio" name='y'/>YD
+                    </div>
+                </div>
+                <div className='form_list'> 지원동기 <br/>
+                <p>CAN 에 지원하게 된 동기와 함께 하고 싶은 활동이 있다면
+                자유롭게 적어주세요</p>
+                    <input type="text" placeholder="학번" className='form_text'/>
+                </div>
+                <div className='form_list'> 관심분야 <br/>
+                    <input type="text" placeholder="학번" className='form_text'/>
+                </div>
                 
-                
+                <div className='Submit'>
+                    <button className='Submit_buton'>제출하기</button>
+                </div>
 
             </form>
         </div>
