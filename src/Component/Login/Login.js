d359b04a130dd5d568dab142331f0d9660cea30c
Song307 <ssongsyj0203@gmail.com>
2024-09-11 22:05:13 +0900

    u

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,14 +1,46 @@
 import './Login.css'
+import { useEffect } from 'react';
+import Navigator_Login from './Navigator_Login';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Login(){
+    useEffect(() => {
+        AOS.init({                              // AOS 초기화
+          duration: 1000,                       // 애니메이션 지속 시간 설정 (1000ms = 1초)
+          once: true,                           // 스크롤할 때 한 번만 애니메이션 실행
+        });
+      }, []);        
+      
+
     return(
-        <div className="Login">
-            
-            <div className='box'>
-                로그인 : <input type='text'></input> <br/>
-                비밀번호 : <input type='password'></input> <br/>
-                <a href='#'>회원가입</a><button>로그인</button>
+        <div>
+            <Navigator_Login/>
+            <div className="Login" >
+                <div className='box' data-aos="fade-up" data-aos-once='false'>
+
+                    <h1 data-aos="fade-right" data-aos-once='false'> LOGIN</h1>
+
+                    <div className='input_box'me data-aos="fade-left" data-aos-once='false'>
+                        <h3>USERNAME</h3>
+                        <input type='text' placeholder='ID'></input><br/><br/>
+                    </div>
+
+                    <div className='input_box' data-aos="fade-right" data-aos-once='false'>
+                        <h3>PASSWORD</h3>
+                        <input type='password' placeholder='Password'></input> <br/>
+                    </div>
+
+                    <div className='option_box'>
+                        <a href='#' className='register'>회원가입✔</a>
+                        <a href='#' className='findPW'>비밀번호 찾기✔</a>
+                    </div>
+                    
+                    
+                    <button className="btn-hover color-8">로그인</button>
+                </div>
             </div>
+            
             
         </div>
     )
---
+++ src/Component/Login/Navigator_Login.js
@@ -0,0 +1,19 @@
+import './Navigator_Login.css'
+
+
+function Navigator_Login(){
+
+    return(
+        <div className='NavR'>
+            <div className='list'>
+                <li className='logo_CAN'><a href='/'>CAN</a></li>
+                <li><a href='/login'>Login</a></li>
+            </div>
+
+        </div>
+    );
+}
+
+export default Navigator_Login;
+
+
