87d85768e0563e2a6fdaf282d545e1fcda1ec60a
Song307 <ssongsyj0203@gmail.com>
2024-09-16 23:04:27 +0900

    update

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,10 +3,14 @@ import './Footer.css';
 function Footer(){
     return(
         <div className='Footer'>
-            <h3>CAN</h3>
-            <h3>e-mail / 동방 위치 / 회장 이름  </h3>
+            <h3>CAN</h3><span>Founded in 1988</span>
+            
+            <h4>e-mail / 동방 위치 / 회장 이름 /  </h4>
             <a href='https://open.kakao.com/o/sPPICnNg'>https://open.kakao.com/o/sPPICnNg</a>
             <h4>Copyright 2024. CAN. All Rights Reserved</h4>
+            <div class="footer-company-info">
+  
+</div>
         </div>
         
     )    
