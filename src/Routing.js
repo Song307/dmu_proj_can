33b1c34f720c46a045785064c6d6e7a41924e03f
Song307 <ssongsyj0203@gmail.com>
2024-09-23 16:43:39 +0900

    commit

--- src/Routing.js
+++ src/Routing.js
@@ -3,6 +3,7 @@ import Home from './Component/Home/Home';
 import Recruit_form from './Component/Recruit/Recruit_form';
 import Recruit from './Component/Recruit/Recruit';
 import Login from './Component/Login/Login';
+import AdminPage from './Component/AdminPage/AdminPage';
 
 function Routing(){
     return(
@@ -13,6 +14,7 @@ function Routing(){
                     <Route path='/Recruit_form' element={<Recruit_form/>}/>
                     <Route path='login' element={<Login/>}/>
                     <Route path='Recruit' element={<Recruit/>} />
+                    <Route path='adminPage' element={<AdminPage/>} />
                 </Routes>
             </BrowserRouter>
         </div>
