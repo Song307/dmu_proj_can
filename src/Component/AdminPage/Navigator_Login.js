ac0177c091f8ed88a048752858b79f5e93e0bc6a
Song307 <ssongsyj0203@gmail.com>
2024-09-26 12:41:45 +0900

    commit

---
+++ src/Component/AdminPage/Navigator_Login.js
@@ -0,0 +1,19 @@
+import './Navigator_Admin.css'
+
+
+function Navigator_Admin(){
+
+    return(
+        <div className='NavR'>
+            <div className='list'>
+                <li className='logo_CAN'><a href='/'>CAN</a></li>
+                <li><a href='/login'>Login</a></li>
+            </div>
+
+        </div>
+    );
+}
+
+export default Navigator_Admin;
+
+
