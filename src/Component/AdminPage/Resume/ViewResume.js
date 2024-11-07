0ed37cb24407f639db5eb780cd96219e2df71690
Song307 <ssongsyj0203@gmail.com>
2024-11-07 14:02:38 +0900

    commit

--- src/Component/AdminPage/Resume/ViewResume.js
+++ src/Component/AdminPage/Resume/ViewResume.js
@@ -3,64 +3,209 @@ import { useState, useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-function ViewResume(){
+function ViewResume() {
     useEffect(() => {
-        AOS.init({                              // AOS 초기화
-          duration: 1000,                       // 애니메이션 지속 시간 설정 (1000ms = 1초)
-          once: true,                           // 스크롤할 때 한 번만 애니메이션 실행
+        AOS.init({
+            duration: 1000,
+            once: true,
         });
-      }, []);  
+    }, []);
 
-    const resume =[
+    const [selectedResume, setSelectedResume] = useState(null);
+    const [viewMode, setViewMode] = useState("ui"); // "ui" for ResumeUiList, "list" for ResumeList_list
+
+    const resume = [
         {
-            Rid : 1,
-            name : '송하성',
-            studentNumber : 20212127,
-            grade : 2,
-            class : 'YD',
-            reason : '동아리 활동 희망',
-            interest : 'Web',
-            github : '',
+            Rid: 1,
+            name: '송하성',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
         },
         {
-            Rid : 2,
-            name : '김주형',
-            studentNumber : 20212127,
-            grade : 2,
-            class : 'YD',
-            reason : '동아리 활동 희망',
-            interest : 'Web',
-            github : '',
-        }
-        
+            Rid: 2,
+            name: '김주형',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
+        {
+            Rid: 1,
+            name: '송하성',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
+        {
+            Rid: 2,
+            name: '김주형',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
+        {
+            Rid: 1,
+            name: '송하성',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
+        {
+            Rid: 2,
+            name: '김주형',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
+        {
+            Rid: 1,
+            name: '송하성',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
+        {
+            Rid: 2,
+            name: '김주형',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
+        {
+            Rid: 1,
+            name: '송하성',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
+        {
+            Rid: 2,
+            name: '김주형',
+            studentNumber: 20212127,
+            grade: 2,
+            class: 'YD',
+            reason: '동아리 활동 희망',
+            interest: 'Web',
+            github: '',
+            call: '01012345678'
+        },
     ];
 
-    function ResumeList(){
-        return(
-            <div className='User_block' data-aos="fade-left" data-aos-once='true'  data-aos-easing="ease-in-out">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-                </svg>
-                <p>{resume[0].Rid} {resume[0].name}</p>
+    function viewDetail(selectedResumeData) {
+        return (
+            <div className='Resume_block'>
+                <section className='resumeId'>
+                    <p>지원서 ID: {selectedResumeData.Rid}</p>
+                    <p className='btnClose' onClick={() => setSelectedResume(null)}>x</p> {/* Close button */}
+                </section>
+                <p>이름: {selectedResumeData.name}</p>
+                <p>학번: {selectedResumeData.studentNumber}</p>
+                <p>학년: {selectedResumeData.grade}</p>
+                <p>반: {selectedResumeData.class}</p>
+                <p>지원 이유: {selectedResumeData.reason}</p>
+                <p>관심 분야: {selectedResumeData.interest}</p>
+                <p>Github: {selectedResumeData.github ? selectedResumeData.github : '없음'}</p>
+                <p>전화번호: {selectedResumeData.call ? selectedResumeData.call : '없음'}</p>
             </div>
-        )
+        );
     }
-    return(
+
+    function ResumeList_list() {
+        return (
+            <div className='return_list'>
+                <div className='title'>
+                    <div className='id'>ID</div>
+                    <div className='name'>NAME</div>
+                    <div className='call'>CALL</div>
+                </div>
+                {resume.map((item) => (
+                    <div className='item' key={item.Rid} onClick={() => setSelectedResume(item.Rid)}>
+                        <div className='id'>{item.Rid}</div>
+                        <div className='name'>{item.name}</div>
+                        <div className='call'>{item.call}</div>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+
+    function ResumeUiList() {
+        return (
+            <div className='ResumeUiList'>
+                {resume.map((item) => (
+                    <div
+                        className='User_block'
+                        key={item.Rid}
+                        onClick={() => setSelectedResume(item.Rid)}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        </svg>
+                        <p>{item.Rid} {item.name}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    
+
+    const selectedResumeData = resume.find(item => item.Rid === selectedResume);
+
+    return (
         <div className='ViewResume'>
             <h1>지원서 조회</h1>
-            <div className='ResumeList_section' > 
-                <ResumeList/>
+            <div className='selectViewMethod'>
+                <section className='viewUiMode' onClick={() => setViewMode("ui")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                    </svg>
+                </section>
+                <section className='viewListMode' onClick={() => setViewMode("list")}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                </svg>
+                </section>
             </div>
-            <div className='Resume_block'>
-                <p>지원서 ID: {resume[0].Rid}</p>
-                <p>이름: {resume[0].name}</p>
-                <p>학번: {resume[0].studentNumber}</p>
-                <p>학년: {resume[0].grade}</p>
-                <p>반: {resume[0].class}</p>
-                <p>지원 이유: {resume[0].reason}</p>
-                <p>관심 분야: {resume[0].interest}</p>
-                <p>Github: {resume[0].github ? resume[0].github : '없음'}</p>
-            </div>
+            {viewMode === "ui" ? <ResumeUiList /> : <ResumeList_list />}
+            
+            {selectedResumeData && viewDetail(selectedResumeData)}
         </div>
     );
 }
--- src/Component/Recruit/Recruit_form.js
+++ src/Component/Recruit/Recruit_form.js
@@ -21,38 +21,38 @@ function Recruit_form(){
             <h2 data-aos="fade-right" data-aos-once='true'>CAN Recruit Form</h2>
             <form className='form' data-aos="fade-left" data-aos-once='true'>
                 <div className='form_list'> 이름 <br/>
-                    <input type="text" placeholder="이름" className='form_text'/>
+                    <input type="text" placeholder="이름" className='form_text' name='name'/>
                 </div>
                 <div className='form_list'> 학번 <br/>
-                    <input type="text" placeholder="학번" className='form_text'/>
+                    <input type="text" placeholder="학번" className='form_text' name='studentId'/>
                 </div>
                 <div className='form_list'> 학년
                     <div className='form_input_list'>
-                    <input className='radio_text' type="radio" name='g'/>1학년<br/>
-                    <input className='radio_text' type="radio" name='g'/>2학년
+                    <input className='radio_text' type="radio" name='1'/>1학년<br/>
+                    <input className='radio_text' type="radio" name='2'/>2학년
                     </div>
                 </div>
                 <div className='form_list'> 분반<br/>
                     <div className='form_input_list'>
-                        <input className='radio_text' type="radio" name='y'/>YA<br/>
-                        <input className='radio_text' type="radio" name='y'/>YB<br/>
-                        <input className='radio_text' type="radio" name='y'/>YC<br/>
-                        <input className='radio_text' type="radio" name='y'/>YD
+                        <input className='radio_text' type="radio" name='class'/>YA<br/>
+                        <input className='radio_text' type="radio" name='class'/>YB<br/>
+                        <input className='radio_text' type="radio" name='class'/>YC<br/>
+                        <input className='radio_text' type="radio" name='class'/>YD
                     </div>
                 </div>
                 <div className='form_list'> 지원동기 <br/>
                 <p>* CAN 에 지원하게 된 동기와 함께 하고 싶은 활동이 있다면 자유롭게 적어주세요</p>
-                    <input type="text" placeholder="지원동기" className='form_text'/>
+                    <input type="text" placeholder="지원동기" className='form_text' name='지원동기'/>
                 </div>
                 <div className='form_list'> 관심분야 <br/>
                 <p>* ex) 게임, 웹 </p>
-                    <input type="text" placeholder="관심분야" className='form_text'/>
+                    <input type="text" placeholder="관심분야" className='form_text' name='관심분야'/>
                 </div>
                 <div className='form_list'> 본인이 했던 프로젝트, 공부한 내용이 있다면 깃허브링크를 첨부해주세요. 없다면 비워두셔도 좋습니다. <br/>
-                    <input type="text" placeholder="git.hub" className='form_text'/>
+                    <input type="text" placeholder="git.hub" className='form_text' name='깃허브링크'/>
                 </div>
                 <div className='form_list'> 전화번호
-                    <input type="text" className='form_text' placeholder="-없이 입력해주세요."/>
+                    <input type="text" className='form_text' placeholder="-없이 입력해주세요." name='num'/>
                 </div>
                 
                 <div className='Submit'>
